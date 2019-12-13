fa105838fa82e076fd95141afe5da24c1335263a
Sujith Surendran <Sujith.Surendran@gds.ey.com>
2019-12-13 15:20:59 +0530

    fix for build issues

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import reducers from "./store/reducers";
+import reducers from "./store/Reducers";
 import thunk from "redux-thunk";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/react-grid-layout/css/styles.css";
