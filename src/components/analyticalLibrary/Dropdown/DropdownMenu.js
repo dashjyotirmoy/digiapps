88026d8a98a85fe8523a018788608ee34f770bab
Aditya Shetty <Aditya.shetty1@gds.ey.com>
2020-03-03 14:47:52 +0530

    CHanges of filter and scroll in dropdown

--- src/components/analyticalLibrary/Dropdown/DropdownMenu.js
+++ src/components/analyticalLibrary/Dropdown/DropdownMenu.js
@@ -7,12 +7,25 @@ const CustomMenu = React.forwardRef(
     const [value, setValue] = useState("");
 
     return (
-      <div ref={ref} className={className} aria-labelledby={labeledBy}>
+      <div
+        ref={ref}
+        className={className}
+        aria-labelledby={labeledBy}
+      >
+        <FormControl
+          autofocus
+          className="mx-3 my-2 w-auto"
+          placeholder="Type to filter..."
+          onChange={e => setValue(e.target.value)}
+          value={value}
+        />
         <ul className="list-unstyled m-0 text-white-50">
-          {React.Children.toArray(children).filter(
-            child =>
-              !value || child.props.children.toLowerCase().startsWith(value)
-          )}
+          <li>
+            {React.Children.toArray(children).filter(
+              child =>
+                !value || child.props.children.toLowerCase().startsWith(value) || child.props.children.startsWith(value)
+            )}
+          </li>
         </ul>
       </div>
     );
