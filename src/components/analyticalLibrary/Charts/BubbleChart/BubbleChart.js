c6a48f31c2d05114b8c0fc39102d92c0a73ae7eb
Aditya Shetty <Aditya.shetty1@gds.ey.com>
2020-04-28 19:01:55 +0530

    Bubble chart binding data of all charts

--- src/components/analyticalLibrary/Charts/BubbleChart/BubbleChart.js
+++ src/components/analyticalLibrary/Charts/BubbleChart/BubbleChart.js
@@ -5,7 +5,13 @@ import { connect } from "react-redux";
 import { color } from "highcharts";
 
 var BubbleChartData;
-var bugsData, vulnearbilityData, codeSmellsData, complexityData, duplicationData, veryLowData = [], lowData = [], mediumData = [], highData = [], criticalData = [];
+var bugsData, vulnearbilityData, codeSmellsData, complexityData, duplicationData;
+var veryLowData = [], lowData = [], mediumData = [], highData = [], criticalData = [];
+var bd = {}, vd = {}, cd = {};
+var veryLowBug = [], lowBug = [], mediumBug = [], highBug = [], criticalBug = [];
+var veryLowVulnearbility = [], lowVulnearbility = [], mediumVulnearbility = [], highVulnearbility = [], criticalVulnearbility = [];
+var veryLowCodeSmells = [], lowCodeSmells = [], mediumCodeSmells = [], highCodeSmells = [], criticalCodeSmells = [];
+
 
 class BubbleHigh extends Component {
   state = {
@@ -84,28 +90,28 @@ class BubbleHigh extends Component {
       },
       series: [
         {
-          name: "VeryLow",
-          data: veryLowData,
-          color: "#90ee90"
-        },
-        {
-          name: "Low",
-          data: lowData,
+          name: "",
+          data: '',
           color: "#056642"
         },
         {
-          name: "Medium",
-          data: mediumData,
+          name: "",
+          data: '',
+          color: "#90ee90"
+        },
+        {
+          name: "",
+          data: '',
           color: "#ffff00"
         },
         {
-          name: "High",
-          data: highData,
+          name: "",
+          data: '',
           color: "#ffa500"
         },
         {
-          name: "Critical",
-          data: criticalData,
+          name: "",
+          data: '',
           color: "#ff0000"
         }
       ]
@@ -113,60 +119,59 @@ class BubbleHigh extends Component {
   };
 
   setBugsData = () => {
-    console.log("Bugs Data");
 
     bugsData = BubbleChartData.map((item, index) => {
       return {
         x: parseInt(item[3].value),
         y: parseInt(item[2].value),
-        z: parseInt(item[0].value)
+        z: parseInt(item[0].value),
+        severity: parseInt(item[1].value)
       }
     }
     )
   };
 
   setVulnerabilityData = () => {
-    console.log("security_rating");
 
     vulnearbilityData = BubbleChartData.map((item, index) => {
       return {
         x: parseInt(item[3].value),
         y: parseInt(item[2].value),
-        z: parseInt(item[1].value)
+        z: parseInt(item[1].value),
+        severity: parseInt(item[0].value)
       }
     }
     )
   };
 
   setCodeSmellsData = () => {
-    console.log("code_smells");
 
     codeSmellsData = BubbleChartData.map((item, index) => {
       return {
         x: parseInt(item[0].value),
-        y: parseInt(item[2].value),
-        z: parseInt(item[1].value)
+        y: parseInt(item[3].value),
+        z: parseInt(item[1].value),
+        severity: parseInt(item[2].value)
       }
     }
     )
   };
 
   setComplexityData = () => {
-    console.log("complexity");
 
     complexityData = BubbleChartData.map((item, index) => {
+      // if (item[0] && item[1] && item[2]) {
       return {
-        x: parseInt(item[1].value),
-        y: parseInt(item[2].value),
-        z: parseInt(item[0].value)
+        x: item[1] ? parseInt(item[1].value) : 0,
+        y: item[2] ? parseFloat(item[2].value) : 0,
+        z: item[0] ? parseInt(item[0].value) : 0
       }
-
+      // }
     }
     )
   };
 
   setDuplicationData = () => {
-    console.log("duplication_lines");
 
     duplicationData = BubbleChartData.map((item, index) => {
       return {
@@ -176,47 +181,8 @@ class BubbleHigh extends Component {
       }
     }
     )
-  };
-
-  setColorIndication = (data) => {
-
-    var coloredBugData = data.map((item, index) => {
-      return {
-        ...item,
-        color: 'red'
-      }
-    }
-
-    )
-    console.log(coloredBugData);
 
   };
-
-  filterBugData = (data) => {
-    data.map((item, index) => {
-      if (item.z == 0) {
-        console.log(item);
-        veryLowData.push(item);
-        console.log(veryLowData);
-      }
-      else if (item.z == 1) {
-        lowData.push(item)
-        console.log(lowData);
-
-      }
-      else if (item.z == 2) {
-        // mediumData.push(item)
-      }
-      else if (item.z == 3) {
-        // highData.push(item)
-      }
-      else {
-        // criticalData.push(item)
-      }
-
-    }
-    )
-  }
 
   render() {
     if (this.props.qualityDrilledDownData.components) {
@@ -228,42 +194,229 @@ class BubbleHigh extends Component {
             value: ele.value
           };
         });
-        // x: item.measures[3].value,
-        // y: item.measures[2].value,
-        // z: item.measures[0].value
-
-
-
       });
+
+
       console.log(BubbleChartData);
 
       if (BubbleChartData[0][0].id == 'bugs') {
         this.setBugsData();
-        this.filterBugData(bugsData);
+        bugsData.map((item, index) => {
+          if (item.severity == 1) {
+            Object.assign(bd, item);
+            veryLowBug.push(bd);
+            bd = {};
+          }
+          else if (item.severity == 2) {
+            Object.assign(bd, item);
+            lowBug.push(bd);
+            bd = {};
+          }
+          else if (item.severity == 3) {
+            Object.assign(bd, item);
+            mediumBug.push(bd);
+            bd = {};
+          }
+          else if (item.severity == 4) {
+            Object.assign(bd, item);
+            highBug.push(bd);
+            bd = {};
+          }
+          else if (item.severity == 5) {
+            Object.assign(bd, item);
+            criticalBug.push(bd);
+            bd = {};
+          }
+        }
+        )
 
+        var bugsDataSeries = [
+          {
+            name: "Critical",
+            data: criticalBug,
+            color: '#ff0000'
+          },
+          {
+            name: "High",
+            data: highBug,
+            color: '#ffa500'
+          },
+          {
+            name: 'Medium',
+            data: mediumBug,
+            color: '#ffff00'
+          },
+          {
+            name: 'Low',
+            data: lowBug,
+            color: '#90ee90'
+          },
+          {
+            name: 'Very Low',
+            data: veryLowBug,
+            color: '#056642'
+          }
+        ]
+
+        this.state.options.series = bugsDataSeries;
 
       }
+
       else if (BubbleChartData[0][0].id == 'security_rating') {
         this.setVulnerabilityData();
-        this.filterBugData(vulnearbilityData);
 
+        vulnearbilityData.map((item, index) => {
 
+          if (item.severity == 1) {
+            Object.assign(vd, item);
+            veryLowVulnearbility.push(vd);
+            vd = {};
+          }
+          else if (item.severity == 2) {
+            Object.assign(vd, item);
+            lowVulnearbility.push(vd);
+            vd = {};
+          }
+          else if (item.severity == 3) {
+            Object.assign(vd, item);
+            mediumVulnearbility.push(vd);
+            vd = {};
+          }
+          else if (item.severity == 4) {
+            Object.assign(vd, item);
+            highVulnearbility.push(vd);
+            vd = {};
+          }
+          else if (item.severity == 5) {
+            Object.assign(vd, item);
+            criticalVulnearbility.push(vd);
+            vd = {};
+          }
+        })
+
+        var vulnearbilityDataSeries = [
+          {
+            name: "Critical",
+            data: criticalVulnearbility,
+            color: '#ff0000'
+          },
+          {
+            name: "High",
+            data: highVulnearbility,
+            color: '#ffa500'
+          },
+          {
+            name: "Medium",
+            data: mediumVulnearbility,
+            color: '#ffff00'
+          },
+          {
+            name: 'Low',
+            data: lowVulnearbility,
+            color: '#90ee90'
+          },
+          {
+            name: 'Very Low',
+            data: veryLowVulnearbility,
+            color: '#056642'
+          }
+        ]
+        this.state.options.series = vulnearbilityDataSeries
       }
+
       else if (BubbleChartData[0][1].id == 'code_smells') {
         this.setCodeSmellsData();
-        this.filterBugData(codeSmellsData)
-      }
-      else if (BubbleChartData[0][1].id == 'complexity') {
-        // this.setComplexityData();
-        // this.filterBugData(complexityData);
-        console.log(BubbleChartData);
+        // this.filterBugData(codeSmellsData);
+        // this.filterCodeSmellsData(codeSmellsData);
+        console.log(codeSmellsData);
+
+        codeSmellsData.map((item, index) => {
+          if (item.severity == 1) {
+            Object.assign(cd, item);
+            veryLowCodeSmells.push(cd)
+            cd = {};
+          }
+          else if (item.severity == 2) {
+            Object.assign(cd, item);
+            lowCodeSmells.push(cd);
+            cd = {};
+          }
+          else if (item.severity == 3) {
+            Object.assign(cd, item);
+            mediumCodeSmells.push(cd);
+            cd = {};
+          }
+          else if (item.severity == 4) {
+            Object.assign(cd, item);
+            highCodeSmells.push(cd);
+            cd = {};
+          }
+          else if (item.severity == 5) {
+            Object.assign(cd, item);
+            criticalCodeSmells.push(cd);
+            cd = {};
+          }
+        }
+        )
+
+        var codeSemllSeries = [
+          {
+            name: "VeryLow",
+            data: veryLowCodeSmells,
+            color: '#056642'
+          },
+          {
+            name: "Low",
+            data: lowCodeSmells,
+            color: '#90ee90'
+          },
+          {
+            name: "Medium",
+            data: mediumCodeSmells,
+            color: '#ffff00'
+          },
+          {
+            name: "High",
+            data: highCodeSmells,
+            color: '#ffa500'
+          },
+          {
+            name: "Critical",
+            data: criticalCodeSmells,
+            color: '#ff0000'
+          }
+        ]
+
+        this.state.options.series = codeSemllSeries
 
       }
+
+      else if (BubbleChartData[0][1].id == 'complexity') {
+        this.setComplexityData();
+
+        var complexityDataSeries = [
+          {
+            name: "Coverage",
+            data: complexityData,
+            color: '#4b9fd5'
+          }
+        ]
+        //complexity is coverage
+        this.state.options.series = complexityDataSeries;
+
+      }
+
       else if (BubbleChartData[0][1].id == 'duplicated_lines') {
         this.setDuplicationData();
-        this.filterBugData(duplicationData);
+        var duplicationDataSeries = [
+          {
+            name: "Duplication",
+            data: duplicationData,
+            color: '#4b9fd5'
+          }
+        ]
+        this.state.options.series = duplicationDataSeries;
       }
-      // this.setColorIndication(bugsData);
 
     }
     else {
