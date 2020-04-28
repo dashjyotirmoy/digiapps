8b13d771c1b8c1620bb34e4d2e9e53c817c255af
Aditya Shetty <Aditya.shetty1@gds.ey.com>
2020-04-28 09:18:26 +0530

    Bubbe chart data binding in class component

--- src/components/analyticalLibrary/Charts/BubbleChart/BubbleChart.js
+++ src/components/analyticalLibrary/Charts/BubbleChart/BubbleChart.js
@@ -3,6 +3,10 @@ import HighchartsReact from "highcharts-react-official";
 import ChartHOC from "../ChartHOC/ChartHOC";
 import { connect } from "react-redux";
 import { color } from "highcharts";
+
+var BubbleChartData;
+var bugsData, vulnearbilityData, codeSmellsData, complexityData, duplicationData, veryLowData = [], lowData = [], mediumData = [], highData = [], criticalData = [];
+
 class BubbleHigh extends Component {
   state = {
     options: {
@@ -80,34 +84,192 @@ class BubbleHigh extends Component {
       },
       series: [
         {
+          name: "VeryLow",
+          data: veryLowData,
+          color: "#90ee90"
+        },
+        {
           name: "Low",
-          data: [
-            { x: 20, y: 20, z: 20 },
-            { x: 10, y: 3, z: 10 }
-          ],
-          color: "#00AA00"
+          data: lowData,
+          color: "#056642"
         },
         {
           name: "Medium",
-          data: [
-            { x: 30, y: 180, z: 760 },
-            { x: 50, y: 145, z: 700 }
-          ],
-          color: "#F0D04D"
+          data: mediumData,
+          color: "#ffff00"
         },
         {
           name: "High",
-          data: [
-            { x: 50, y: 50, z: 400 },
-            { x: 70, y: 80, z: 500 }
-          ],
-          color: "#F3413E"
+          data: highData,
+          color: "#ffa500"
+        },
+        {
+          name: "Critical",
+          data: criticalData,
+          color: "#ff0000"
         }
       ]
     }
   };
+
+  setBugsData = () => {
+    console.log("Bugs Data");
+
+    bugsData = BubbleChartData.map((item, index) => {
+      return {
+        x: parseInt(item[3].value),
+        y: parseInt(item[2].value),
+        z: parseInt(item[0].value)
+      }
+    }
+    )
+  };
+
+  setVulnerabilityData = () => {
+    console.log("security_rating");
+
+    vulnearbilityData = BubbleChartData.map((item, index) => {
+      return {
+        x: parseInt(item[3].value),
+        y: parseInt(item[2].value),
+        z: parseInt(item[1].value)
+      }
+    }
+    )
+  };
+
+  setCodeSmellsData = () => {
+    console.log("code_smells");
+
+    codeSmellsData = BubbleChartData.map((item, index) => {
+      return {
+        x: parseInt(item[0].value),
+        y: parseInt(item[2].value),
+        z: parseInt(item[1].value)
+      }
+    }
+    )
+  };
+
+  setComplexityData = () => {
+    console.log("complexity");
+
+    complexityData = BubbleChartData.map((item, index) => {
+      return {
+        x: parseInt(item[1].value),
+        y: parseInt(item[2].value),
+        z: parseInt(item[0].value)
+      }
+
+    }
+    )
+  };
+
+  setDuplicationData = () => {
+    console.log("duplication_lines");
+
+    duplicationData = BubbleChartData.map((item, index) => {
+      return {
+        x: parseInt(item[2].value),
+        y: parseInt(item[1].value),
+        z: parseInt(item[0].value)
+      }
+    }
+    )
+  };
+
+  setColorIndication = (data) => {
+
+    var coloredBugData = data.map((item, index) => {
+      return {
+        ...item,
+        color: 'red'
+      }
+    }
+
+    )
+    console.log(coloredBugData);
+
+  };
+
+  filterBugData = (data) => {
+    data.map((item, index) => {
+      if (item.z == 0) {
+        console.log(item);
+        veryLowData.push(item);
+        console.log(veryLowData);
+      }
+      else if (item.z == 1) {
+        lowData.push(item)
+        console.log(lowData);
+
+      }
+      else if (item.z == 2) {
+        // mediumData.push(item)
+      }
+      else if (item.z == 3) {
+        // highData.push(item)
+      }
+      else {
+        // criticalData.push(item)
+      }
+
+    }
+    )
+  }
+
   render() {
-    console.log('ddddddddddddddddddwwwwwwwwwwwwssssssssssss', this.props.qualityDrilledDownData);
+    if (this.props.qualityDrilledDownData.components) {
+      console.log('ddddddddddddddddddwwwwwwwwwwwwssssssssssss', this.props.qualityDrilledDownData.components);
+      BubbleChartData = this.props.qualityDrilledDownData.components.map((item, index) => {
+        return item.measures.map(ele => {
+          return {
+            id: ele.metric,
+            value: ele.value
+          };
+        });
+        // x: item.measures[3].value,
+        // y: item.measures[2].value,
+        // z: item.measures[0].value
+
+
+
+      });
+      console.log(BubbleChartData);
+
+      if (BubbleChartData[0][0].id == 'bugs') {
+        this.setBugsData();
+        this.filterBugData(bugsData);
+
+
+      }
+      else if (BubbleChartData[0][0].id == 'security_rating') {
+        this.setVulnerabilityData();
+        this.filterBugData(vulnearbilityData);
+
+
+      }
+      else if (BubbleChartData[0][1].id == 'code_smells') {
+        this.setCodeSmellsData();
+        this.filterBugData(codeSmellsData)
+      }
+      else if (BubbleChartData[0][1].id == 'complexity') {
+        // this.setComplexityData();
+        // this.filterBugData(complexityData);
+        console.log(BubbleChartData);
+
+      }
+      else if (BubbleChartData[0][1].id == 'duplicated_lines') {
+        this.setDuplicationData();
+        this.filterBugData(duplicationData);
+      }
+      // this.setColorIndication(bugsData);
+
+    }
+    else {
+      console.log('Not defines yet!!');
+
+    }
     this.state.options.title.text = this.props.title;
     return (
       <React.Fragment>
