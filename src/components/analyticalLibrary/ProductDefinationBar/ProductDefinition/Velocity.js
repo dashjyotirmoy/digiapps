3131170587e0ff2e8111fdcb54b8e522bab56990
Ramu D <Ramu.D@gds.ey.com>
2020-05-19 13:00:54 +0530

    initial commit.

--- src/components/analyticalLibrary/ProductDefinationBar/ProductDefinition/Velocity.js
+++ src/components/analyticalLibrary/ProductDefinationBar/ProductDefinition/Velocity.js
@@ -6,21 +6,30 @@ import ControlChartHigh from "../../Charts/ControlChartHigh/ControlChartHigh";
 import { chartDataDispatch } from "../../../../store/actions/chartData";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faSquare
+} from "@fortawesome/free-solid-svg-icons";
 import VelocityTrend from "../../Charts/VelocityTrends/VelocityTrend";
 import SprintBurndown from "../../Charts/SprintBurnDown/SprintBurnDown";
 import Spinner from "../../Spinner/Spinner";
 import BreakDownHigh from "../../Charts/ProjectBreakDown/ProjectBreakDown";
 import { translations } from "../../Translations/Translations";
 import Layout from "../../../../utility/layoutManager/layoutManager";
+import { Row, Col,Button} from "react-bootstrap";
+import Dropdown from "../../Dropdown/Dropdown";
 
 class Velocity extends Component {
   state = {
     charts: [],
+    showbutton: true,
     layout: {
       lg: [],
       md: []
     },
     gridCol: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+   dropData : [{ id: "all_time", name: "All Time" }, { id: "last_1_month", name: "Last Month" }, { id: "last_3_month", name: "Last 3 Months" }],
     gridBreakpoints: { lg: 1200, md: 768, sm: 576, xs: 480, xxs: 0 },
     show: true
   };
@@ -189,12 +198,97 @@ class Velocity extends Component {
       });
   };
 
+  handleRepoChange = repoID => {
+  
+  };
+
+  setCode = () => {
+    
+  }
+
+  setBuild = () => {
+    
+  }
+
+
   render() {
     if (this.state.show) {
       return <Spinner show="true" />;
     } else {
       return (
         <React.Fragment>
+          <Row className="p-0 px-3 m-0 mt-4 mb-3 d-flex justify-content-start">
+     
+     <Col md={2}>
+       <Dropdown
+         listData={this.state.dropData}
+         direction="down"
+         onSelectDelegate={this.handleRepoChange}
+       >
+         <Row className="h-100 bg-prodAgg-btn repo-height m-0 p-0 rounded">
+           <Col
+             sm={10}
+             md={10}
+             lg={10}
+             xl={10}
+             className="d-flex align-item-center justify-content-center"
+           >
+             <p className="font-aggegate-sub-text text-ellipsis font-weight-bold text-white m-auto text-left text-lg-left text-md-left text-sm-left text-xl-center">
+               {this.state.selectedRepo
+                 ? this.state.selectedRepo
+                 : "Select Project"}
+             </p>
+           </Col>
+           <Col
+             sm={2}
+             md={2}
+             g={2}
+             xl={2}
+             className="font-aggegate-sub-text p-0 text-white d-flex align-items-center"
+           >
+             <FontAwesomeIcon icon={faChevronDown} />
+           </Col>
+         </Row>
+       </Dropdown>
+     </Col>
+<Col md={8}>
+     <span>
+     {this.state.showbutton ? (
+       <Button variant="outline-dark" className={this.state.alertActive?"bgblue":"Alertbg"}  onClick ={this.setCode}>Code</Button>
+   //  <button className="bg-prodAgg-btn" style={{ color: '#FFFFFF', background: '#1D2632', border: '#364D68', minWidth: '6rem' }} onClick ={this.setAlert} >Alert</button>
+   ) : null}
+</span>
+  
+<span className="ml-3">
+{this.state.showbutton ? (
+      <Button variant="outline-dark"  className={this.state.policyActive?"bgblue":"Alertbg"} onClick ={this.setBuild}>Build</Button>
+   //  <button className="bg-prodAgg-btn" style={{ color: '#FFFFFF', paddingLeft: '5px', background: '#1D2632', border: '#364D68', minWidth: '6rem' }} onClick ={this.setPolicy} >Policy</button>
+   ) : null}
+</span>
+</Col>
+{/* { policyLegend === true?(
+<Col md={2} className="pt-3">
+<div>
+<span className="mr-3">
+<FontAwesomeIcon  className="highbg" icon={faSquare} />
+<span style={{color:'#fff'}}>High</span>
+</span>
+<span className="mr-3">
+<FontAwesomeIcon  className="mediumbg" icon={faSquare} />
+<span style={{color:'#fff'}}>Medium</span>
+</span>
+<span className="mr-3">
+<FontAwesomeIcon  className="lowbg" icon={faSquare} />
+<span style={{color:'#fff'}}>Low</span>
+</span>
+</div>
+
+</Col>
+):<div></div>
+} */}
+
+
+   </Row>
           {this.state.charts.length > 0 ? (
             <Grid
               chartData={this.state.charts}
